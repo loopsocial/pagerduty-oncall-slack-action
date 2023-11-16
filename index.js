24efde58abda6a53819a21264e2f6ebdcd2887f7
Deric Walintukan <deric@loopapps.com>
2023-11-16 00:13:54 -0800

    change func getting on-call name to opsgenie

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const core = require('@actions/core')
-const axios = require('axios')
-const { api } = require('@pagerduty/pdjs')
+const core = require("@actions/core");
+const axios = require("axios");
+
+const OPSGENIE_API_BASE_URL = "https://api.opsgenie.com/v2";
 
 /**
  * Gets the input from the used action.
@@ -8,24 +9,33 @@ const { api } = require('@pagerduty/pdjs')
  * @returns {string} Returns the value of the input.
  */
 const getInput = (key) => {
-  const input = core.getInput(key)
-  if (!input) throw Error(`Input "${key}" was not defined`)
-  return input
-}
+  const input = core.getInput(key);
+  if (!input) throw Error(`Input "${key}" was not defined`);
+  return input;
+};
 
 /**
- * Get the PagerDuty on-call name via the API.
- * @returns {string} Returns the PagerDuty on-call name.
+ * Get the OpsGenie on-call name via the API.
+ * @returns {string} Returns the OpsGenie Schedule on-call name.
  */
- const getPagerDutyOnCallName = async () => {
-  const apiToken = core.getInput('pagerduty-api-token')
-  const scheduleId = core.getInput('pagerduty-schedule-id')
-  
-  const pd = api({ token: apiToken })
-  const { data: { oncalls } } = await pd.get(`/oncalls?schedule_ids%5B%5D=${scheduleId}`)
-  if (oncalls.length === 0) throw Error('Schedule ID has no on-calls.')
-  return oncalls[0].user.summary
-}
+const getOpsGenieOnCallName = async () => {
+  const apiToken = core.getInput("opsgenie-api-token");
+  const scheduleId = core.getInput("opsgenie-schedule-id");
+
+  // Gets the on-call name from OpsGenie API
+  const url = `${OPSGENIE_API_BASE_URL}/schedules/${scheduleId}/on-calls`;
+  const {
+    data: { onCallParticipants: onCalls },
+  } = await axios.get(url, {
+    headers: {
+      Authorization: `GenieKey ${apiToken}`,
+    },
+  });
+
+  // Handle API response
+  if (!onCalls || !onCalls.length) throw Error("No On-Call found for Schedule");
+  return onCalls[0].name;
+};
 
 /**
  * Constructs the Slack message JSON payload.
@@ -34,46 +44,46 @@ const getInput = (key) => {
  */
 const constructSlackMessage = (onCallName) => {
   return {
-    "blocks": [
+    blocks: [
       {
-        "type": "header",
-        "text": {
-          "type": "plain_text",
-          "text": `${onCallName}, you are now on active duty as the Release Cop 🚔`
-        }
+        type: "header",
+        text: {
+          type: "plain_text",
+          text: `${onCallName}, you are now on active duty as the Release Cop 🚔`,
+        },
       },
       {
-        "type": "section",
-        "text": {
-          "type": "mrkdwn",
-          "text": "@here Please fulfill your duty in making sure the current Release Candidate is in motion and unblocked."
-        }
-      }
-    ]
-  }
-}
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: "@here Please fulfill your duty in making sure the current Release Candidate is in motion and unblocked.",
+        },
+      },
+    ],
+  };
+};
 
 /**
  * Posts to Slack via webhook.
  * @param {object} body Body to post to Slack.
  */
- const postToSlack = async (body) => {
-  const webhookUrl = getInput('slack-webhook-url')
-  await axios.post(webhookUrl, body)
-}
+const postToSlack = async (body) => {
+  const webhookUrl = getInput("slack-webhook-url");
+  await axios.post(webhookUrl, body);
+};
 
 const run = async () => {
   try {
     // Get the current on-call name
-    const onCallName = await getPagerDutyOnCallName()
+    const onCallName = await getOpsGenieOnCallName();
 
     // Construct Slack message
-    const message = constructSlackMessage(onCallName)
+    const message = constructSlackMessage(onCallName);
 
     // Post to Slack
-    await postToSlack(message)
+    await postToSlack(message);
   } catch (error) {
-    core.setFailed(error.message)
+    core.setFailed(error.message);
   }
-}
-run()
+};
+run();
